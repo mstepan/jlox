2c2b66012dae665d3183c739a9b8f00673b0f51e
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-12-12 16:59:32 +0200

    prettyPrintingWithComparison added

--- src/test/java/org/max/crafting/interpreter/jlox/ast/visitor/PrettyPrinterNodeVisitorTest.java
+++ src/test/java/org/max/crafting/interpreter/jlox/ast/visitor/PrettyPrinterNodeVisitorTest.java
@@ -38,4 +38,36 @@ public class PrettyPrinterNodeVisitorTest {
 
         assertEquals("-6*(2+3)", visitor.getRepresentation());
     }
+
+    @Test
+    void prettyPrintingWithComparison() {
+
+        // 1 - (2 * 3) < 4 == false
+        Expression expression = new BinaryExpression(
+                //1 - (2 * 3) < 4
+                new BinaryExpression(
+                        new Literal(1),
+                        new Token(TokenType.MINUS, null, null, 0),
+
+                        //(2 * 3) < 4
+                        new BinaryExpression(
+                                new Parentheses(new BinaryExpression(
+                                        new Literal(2),
+                                        new Token(TokenType.STAR, null, null, 0),
+                                        new Literal(3))),
+                                new Token(TokenType.LESS, null, null, 0),
+                                new Literal(4))
+                ),
+                new Token(TokenType.EQUAL_EQUAL, null, null, 0),
+                new Literal("false")
+        );
+
+        PrettyPrinterNodeVisitor visitor = new PrettyPrinterNodeVisitor();
+
+        expression.accept(visitor);
+
+        assertEquals("1-(2*3)<4==false", visitor.getRepresentation());
+    }
+
+
 }
