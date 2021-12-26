bdb65a2189862be29f72fa9bb0027828dfe0da1a
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-12-26 15:52:10 +0200

    Unit tests added for Lox script execution interpreter.

---
+++ src/test/java/org/max/crafting/interpreter/jlox/LoxEvalExpressionsTest.java
@@ -0,0 +1,160 @@
+package org.max.crafting.interpreter.jlox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+final class LoxEvalExpressionsTest {
+
+    @Test
+    void divisionByZeroShouldThrowException() {
+        Lox.eval("10/0");
+        assertTrue(Lox.hasError());
+    }
+
+    @Test
+    void comparingNansShouldReturnFalse() {
+        Object res = Lox.eval("0.0/0.0 == 0.0/0.0");
+
+        assertFalse(Lox.hasError());
+        assertEquals("false", res);
+    }
+
+    //==== Concatenation ====
+    @Test
+    void stringConcatenation() {
+        Object res = Lox.eval("\"hello\" + \"world\"");
+        assertFalse(Lox.hasError());
+        assertEquals("helloworld", res);
+    }
+
+    @Test
+    void stringAndNumberConcatenation() {
+        Object res = Lox.eval("\"hello\" + 133");
+        assertFalse(Lox.hasError());
+        assertEquals("hello133", res);
+    }
+
+    @Test
+    void stringAndBooleanConcatenation() {
+        Object res = Lox.eval("\"hello\" + true");
+        assertFalse(Lox.hasError());
+        assertEquals("hellotrue", res);
+    }
+
+    @Test
+    void stringAndNilConcatenation() {
+        Object res = Lox.eval("\"hello\" + nil");
+        assertFalse(Lox.hasError());
+        assertEquals("hellonil", res);
+    }
+
+    @Test
+    void numberAndStringConcatenation() {
+        Object res = Lox.eval("133 + \"str-123\"");
+        assertFalse(Lox.hasError());
+        assertEquals("133str-123", res);
+    }
+
+    @Test
+    void booleanAndStringConcatenation() {
+        Object res = Lox.eval("true + \"str-123\"");
+        assertFalse(Lox.hasError());
+        assertEquals("truestr-123", res);
+    }
+
+    //==== PLUS operator ====
+
+    @Test
+    void plusForIntegers() {
+        Object res = Lox.eval("10+20");
+
+        assertFalse(Lox.hasError());
+        assertEquals("30", res);
+    }
+
+    @Test
+    void plusForFloatingNumbers() {
+        Object res = Lox.eval("10.23 + 20.45");
+
+        assertFalse(Lox.hasError());
+        assertEquals("30.68", res);
+    }
+
+    @Test
+    void multiplePlusOperators() {
+        Object res = Lox.eval("1+2+3+4+5");
+
+        assertFalse(Lox.hasError());
+        assertEquals("15", res);
+    }
+
+    @Test
+    void plusForTwoBooleansShouldFail() {
+        Lox.eval("true + false");
+        assertTrue(Lox.hasError());
+    }
+
+    // ==== expressions ====
+    @Test
+    void singleNumericalExpression() {
+        Object res = Lox.eval("(3*6) + 2");
+        assertFalse(Lox.hasError());
+        assertEquals("20", res);
+    }
+
+    @Test
+    void singleEqEqLogicalExpression() {
+        Object res = Lox.eval("(3*6) + 2 == 20");
+        assertFalse(Lox.hasError());
+        assertEquals("true", res);
+    }
+
+    @Test
+    void singleBangEqLogicalExpression() {
+        Object res = Lox.eval("(3*6) + 2 != 50");
+        assertFalse(Lox.hasError());
+        assertEquals("true", res);
+    }
+
+    @Test
+    void commaSeparatedExpressions() {
+        Object res = Lox.eval("(3*6) + 2 != 50, 2 * 4 == 8");
+        assertFalse(Lox.hasError());
+        assertEquals("true", res);
+    }
+
+    @Test
+    void expressionsWithUnaryMinus() {
+        Object res = Lox.eval("2 * (6 / -3)");
+
+        assertFalse(Lox.hasError());
+        assertEquals("-4", res);
+    }
+
+    @Test
+    void complexExpressionWithGroupingAndUnaryMinus() {
+        Object res = Lox.eval("((10+20) * 33.24) - 177.45 + (-20)");
+
+        assertFalse(Lox.hasError());
+        assertEquals("799.75", res);
+    }
+
+    @Test
+    void groupingWithoutMatchingParenthesesShouldFail() {
+        Lox.eval("(3*6 + 2 != 50");
+
+        assertTrue(Lox.hasError());
+        assertEquals("[line 1] Error: ')' expected", Lox.lastErrorMsg);
+    }
+
+    @Test
+    void negateStringShouldFail() {
+        Lox.eval("2 * (6 / -\"hello, world\")");
+
+        assertTrue(Lox.hasError());
+        assertEquals("[line 1]: Operand must be a number.", Lox.lastErrorMsg);
+    }
+}
--- src/test/java/org/max/crafting/interpreter/jlox/LoxTest.java
+++ src/test/java/org/max/crafting/interpreter/jlox/LoxTest.java
@@ -1,160 +1,235 @@
 package org.max.crafting.interpreter.jlox;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 final class LoxTest {
 
-    @Test
-    void divisionByZeroShouldThrowException() {
-        Lox.eval("10/0");
-        assertTrue(Lox.hasError());
+    private static final ByteArrayOutputStream OUT = new ByteArrayOutputStream();
+    private static final ByteArrayOutputStream ERR = new ByteArrayOutputStream();
+
+    private static String output() {
+        return new String(OUT.toByteArray(), Charset.defaultCharset());
+    }
+
+    private static String errorOutput() {
+        return new String(ERR.toByteArray(), Charset.defaultCharset());
+    }
+
+    @BeforeAll
+    static void setUp() {
+        System.setOut(new PrintStream(OUT));
+        System.setErr(new PrintStream(ERR));
+    }
+
+    @AfterEach
+    void tearDown() {
+        OUT.reset();
+        ERR.reset();
     }
 
     @Test
-    void comparingNansShouldReturnFalse() {
-        Object res = Lox.eval("0.0/0.0 == 0.0/0.0");
+    void runNormalScript() {
 
-        assertFalse(Lox.hasError());
-        assertEquals("false", res);
-    }
+        String script = """
+                // some dummy unused variables declaration
+                var x = 10;
+                var y = 20;
+                var z;
+                            
+                print "x = " + x;
+                print "y = " + y;
+                print "Uninitialized variable is set to: " + z;
+                            
+                // assignment var to var
+                x = y;
+                            
+                print "x = " + x;
+                print "y = " + y;
+                            
+                // assigment var to expression
+                x = (2*3) + 4;
+                            
+                print "x = " + x;
+                            
+                // radius for a circle as int
+                var r = 10;
+                            
+                // PI constant as a double
+                var pi = 3.14;
+                            
+                // printing some message
+                print "calculating area for circle with radius: " + r + " m";
+                            
+                var area = pi * r * r;
+                            
+                print "area: " + area + " m^2";
 
-    //==== Concatenation ====
-    @Test
-    void stringConcatenation() {
-        Object res = Lox.eval("\"hello\" + \"world\"");
-        assertFalse(Lox.hasError());
-        assertEquals("helloworld", res);
-    }
+                """;
 
-    @Test
-    void stringAndNumberConcatenation() {
-        Object res = Lox.eval("\"hello\" + 133");
-        assertFalse(Lox.hasError());
-        assertEquals("hello133", res);
-    }
+        Lox.run(script);
 
-    @Test
-    void stringAndBooleanConcatenation() {
-        Object res = Lox.eval("\"hello\" + true");
-        assertFalse(Lox.hasError());
-        assertEquals("hellotrue", res);
-    }
-
-    @Test
-    void stringAndNilConcatenation() {
-        Object res = Lox.eval("\"hello\" + nil");
-        assertFalse(Lox.hasError());
-        assertEquals("hellonil", res);
-    }
-
-    @Test
-    void numberAndStringConcatenation() {
-        Object res = Lox.eval("133 + \"str-123\"");
-        assertFalse(Lox.hasError());
-        assertEquals("133str-123", res);
-    }
-
-    @Test
-    void booleanAndStringConcatenation() {
-        Object res = Lox.eval("true + \"str-123\"");
-        assertFalse(Lox.hasError());
-        assertEquals("truestr-123", res);
-    }
-
-    //==== PLUS operator ====
-
-    @Test
-    void plusForIntegers() {
-        Object res = Lox.eval("10+20");
-
-        assertFalse(Lox.hasError());
-        assertEquals("30", res);
+        assertFalse(Lox.hasError(), "Unexpected error(-s) detected");
+        assertEquals(
+                """
+                        x = 10
+                        y = 20
+                        Uninitialized variable is set to: nil
+                        x = 20
+                        y = 20
+                        x = 10
+                        calculating area for circle with radius: 10 m
+                        area: 314.0 m^2
+                        """,
+                output());
     }
 
     @Test
-    void plusForFloatingNumbers() {
-        Object res = Lox.eval("10.23 + 20.45");
+    void test123() {
 
-        assertFalse(Lox.hasError());
-        assertEquals("30.68", res);
+        String script = """
+                x = 133;
+                print x;
+                """;
+
+        Lox.run(script);
+
+        assertFalse(Lox.hasError(), "Unexpected error(-s) detected");
+
+        assertEquals(
+                """
+                        133
+                        """,
+                output());
+
+    }
+
+    // ==== VAR declaration ====
+
+    @Test
+    void varDeclaration() {
+
+        String script = """
+                var x = 1;
+                var y = 2;
+                
+                print x;
+                print y;
+                """;
+
+        Lox.run(script);
+
+        assertFalse(Lox.hasError(), "Unexpected error(-s) detected");
+
+        assertEquals(
+                """
+                        1
+                        2
+                        """,
+                output());
+
     }
 
     @Test
-    void multiplePlusOperators() {
-        Object res = Lox.eval("1+2+3+4+5");
+    void redeclareVariableFewTimesShouldBeOK() {
 
-        assertFalse(Lox.hasError());
-        assertEquals("15", res);
+        String script = """
+                var x = 1;
+                var y = 2;
+                var x = 133;
+                
+                print x;
+                print y;
+                
+                var x = 155;
+                print x;
+                """;
+
+        Lox.run(script);
+
+        assertFalse(Lox.hasError(), "Unexpected error(-s) detected");
+
+        assertEquals(
+                """
+                        133
+                        2
+                        155
+                        """,
+                output());
+
+    }
+
+
+    // ==== ASSIGNMENTS ====
+    @Test
+    void assignments() {
+
+        String script = """
+                var x = 1;
+                var y = 2;
+                            
+                var z;
+                                
+                z = x;
+                print "z = " + z;
+                                
+                z = (2*3) + 4;
+                                
+                print "z = " + z;
+
+                """;
+
+        Lox.run(script);
+
+        assertFalse(Lox.hasError(), "Unexpected error(-s) detected");
+
+        assertEquals(
+                """
+                        z = 1
+                        z = 10
+                        """,
+                output());
+
     }
 
     @Test
-    void plusForTwoBooleansShouldFail() {
-        Lox.eval("true + false");
-        assertTrue(Lox.hasError());
-    }
+    void assignmentToStringShouldFail() {
+        String script = """
+                "hello" = 133;
+                """;
 
-    // ==== expressions ====
-    @Test
-    void singleNumericalExpression() {
-        Object res = Lox.eval("(3*6) + 2");
-        assertFalse(Lox.hasError());
-        assertEquals("20", res);
-    }
+        Lox.run(script);
 
-    @Test
-    void singleEqEqLogicalExpression() {
-        Object res = Lox.eval("(3*6) + 2 == 20");
-        assertFalse(Lox.hasError());
-        assertEquals("true", res);
+        assertTrue(Lox.hasError(), "Error wasn't detected, strange");
+        assertEquals(
+                """
+                        [line 1] Error: Invalid assignment target.
+                        """,
+                errorOutput());
     }
 
     @Test
-    void singleBangEqLogicalExpression() {
-        Object res = Lox.eval("(3*6) + 2 != 50");
-        assertFalse(Lox.hasError());
-        assertEquals("true", res);
-    }
+    void assignmentToNumberShouldFail() {
+        String script = """
+                var x = 133;
+                155 = x;
+                """;
 
-    @Test
-    void commaSeparatedExpressions() {
-        Object res = Lox.eval("(3*6) + 2 != 50, 2 * 4 == 8");
-        assertFalse(Lox.hasError());
-        assertEquals("true", res);
-    }
+        Lox.run(script);
 
-    @Test
-    void expressionsWithUnaryMinus() {
-        Object res = Lox.eval("2 * (6 / -3)");
-
-        assertFalse(Lox.hasError());
-        assertEquals("-4", res);
-    }
-
-    @Test
-    void complexExpressionWithGroupingAndUnaryMinus() {
-        Object res = Lox.eval("((10+20) * 33.24) - 177.45 + (-20)");
-
-        assertFalse(Lox.hasError());
-        assertEquals("799.75", res);
-    }
-
-    @Test
-    void groupingWithoutMatchingParenthesesShouldFail() {
-        Lox.eval("(3*6 + 2 != 50");
-
-        assertTrue(Lox.hasError());
-        assertEquals("[line 1] Error: ')' expected", Lox.lastErrorMsg);
-    }
-
-    @Test
-    void negateStringShouldFail() {
-        Lox.eval("2 * (6 / -\"hello, world\")");
-
-        assertTrue(Lox.hasError());
-        assertEquals("[line 1]: Operand must be a number.", Lox.lastErrorMsg);
+        assertTrue(Lox.hasError(), "Error wasn't detected, strange");
+        assertEquals(
+                """
+                        [line 2] Error: Invalid assignment target.
+                        """,
+                errorOutput());
     }
 }
